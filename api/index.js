200a5c362734a5abbb1ef8b4f18c5e825f5c8278
Akshay Thakur <Akshay@acenet.io>
2023-11-30 14:28:09 +0530

    creating model and Database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(`Error connecting to MongoDB: ${error}`);
+  });
 
 const app = express();
 
