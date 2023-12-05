03cb27c772af2b4c4727c630d9865e04650a416e
Akshay Thakur <Akshay@acenet.io>
2023-12-05 15:14:29 +0530

    create update user api

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -21,6 +22,8 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server listening on Port 3000");
